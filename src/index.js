41781b15c85c48ba2f1e71ad0037d63994f43882
burakou_vitali <vitali.burakou@gmail.com>
2021-01-20 03:42:18 +0300

    fix: error index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import './styles/style.scss';
 import './styles/startPage.scss';
 import './styles/mainMenu.scss';
 
-// import gsap from 'gsap';
 import Controller from './js/Controller';
-// import PageLayout from './js/PageLayout';
-// import gameGif from './assets/gameGif.gif';
 
-// const pageLayout = new PageLayout();
-// pageLayout.createStartPage();
-// pageLayout.createModal();
-// pageLayout.createCanvas();
 const controller = new Controller();
 controller.init();
-// controller.startBtnEventListener();
-// controller.spawnEnemies();
-// controller.windowEventListen();
-// controller.drawHead();
-// pageLayout.listenEvents(pageLayout.createScore);
