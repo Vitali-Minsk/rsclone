d051742f6e671e09f982ade1cb278e88b262372f
burakou_vitali <vitali.burakou@gmail.com>
2021-01-20 03:25:51 +0300

    feat: main menu add

--- src/js/particle.js
+++ src/js/particle.js
@@ -1,8 +1,9 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.querySelector('canvas');
+// const ctx = canvas.getContext('2d');
 
 export default class Particle {
-  constructor(x, y, radius, color, velocity) {
+  constructor(ctx, x, y, radius, color, velocity) {
+    this.ctx = ctx;
     this.x = x;
     this.y = y;
     this.radius = radius;
@@ -13,13 +14,13 @@ export default class Particle {
   }
 
   draw() {
-    ctx.save();
-    ctx.globalAlpha = this.alpha;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.restore();
+    this.ctx.save();
+    this.ctx.globalAlpha = this.alpha;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+    this.ctx.restore();
   }
 
   update() {
